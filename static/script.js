9c15a6a9742fc607a6bb7d75a83235aa83de3132
Pavel Bandurin <pavel2012155@gmail.com>
2022-04-04 03:42:58 +0300

    .
    .

--- static/script.js
+++ static/script.js
@@ -1,62 +1,35 @@
-var themonth = 1;
-renderCal(themonth);
+let navbar = $(".navbar");
 
-$('.minusmonth').click(function(){
-  themonth += -1;
-  renderCal(themonth);
-});
-
-$('.addmonth').click(function(){
-  themonth += 1;
-  renderCal(themonth);
-});
-
-function renderCal(themonth){
-$('.calendar li').remove();
-$('.calendar ul').append('<li>Mo</li><li>Tu</li><li>We</li><li>Th</li><li>Fr</li><li>Sa</li> <li>Su</li>');
-var d = new Date(),
-  currentMonth = d.getMonth()+themonth, // get this month
-  days = numDays(currentMonth,d.getYear()), // get number of days in the month
-  fDay = firstDay(currentMonth,d.getYear())-1, // find what day of the week the 1st lands on
-  months = ['January','February','March','April','May','June','July','August','September','October','November','December']; // month names
-
-$('.calendar p.monthname').text(months[currentMonth-1]); // add month name to calendar
-
-for (var i=0;i<fDay-1;i++) { // place the first day of the month in the correct position
-  $('<li>&nbsp;</li>').appendTo('.calendar ul');
-}
-
-for (var i = 1;i<=days;i++) { // write out the days
-  $('<li>'+i+'</li>').appendTo('.calendar ul');
-}
-
-function firstDay(month,year) {
-  return new Date(year,month,1).getDay();
-}
-
-function numDays(month,year) {
-  return new Date(year,month,0).getDate();
-}
-
-var clicker = 0;
-var min = 0;
-var max = 0;
-
-$('.calendar li').click(function(){ // toggle selected dates
-  if(clicker==0){
-    clicker=1;
-    $('.calendar li').removeClass('red');
-    $(this).addClass('red');
-    min = $(this).text();
-  } else {
-    clicker=0;
-    $(this).addClass('red');
-    $('.calendar li.red').each(function(){
-      max = $(this).text();
-    });
-    for(i=parseInt(min);i<parseInt(max);i++){
-      $('.calendar li:nth-of-type('+(i+7+fDay-1)+')').addClass('red'); 
-    }
+$(window).scroll(function(){
+  let oTop = $(".section").offset().top-window.innerHeight;
+  if($(window).scrollTop()>oTop){
+    navbar.addClass("sticky");
+  }else{
+    navbar.removeClass("sticky");
   }
 });
-}
+
+
+let nCount = function(selector){
+
+  $(selector).each(function(){
+    $(this).animate({
+      Counter:$(this).text()
+    },{
+      duration:4000,
+      easing:"swing",
+      step:function(value){
+        $(this).text(Math.ceil(value));
+      }
+    });
+  });
+};
+
+ let a =0;
+$(window).scroll(function(){
+  let oTop = $(".numbers").offset().top-window.innerHeight;
+  if(a==0&&$(window).scrollTop()>=oTop){
+    a++;
+    nCount(".rect>h1");
+  }
+});
